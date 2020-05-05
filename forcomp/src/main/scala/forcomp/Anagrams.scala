fa3412b7a48912ef207c48c126f0f9ea1237f7d3
Peter Savichev (proton) <psavichev@gmail.com>
2020-05-05 22:37:13 +0200

    Anagrams#combinations implemented

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -93,7 +93,21 @@ object Anagrams extends AnagramsInterface {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    def generateCombinations(x: List[List[(Char, Int)]]): List[List[(Char, Int)]] = x match {
+      case Nil    => List(Nil)
+      case h :: _ => h.flatMap(i => generateCombinations(x.tail).map(i :: _))
+    }
+
+    def occurrencesVariety = occurrences.map { occurrency => {
+      (0 to occurrency._2)
+        .map { cnt => (occurrency._1, cnt) }
+        .toList
+    }}
+
+    generateCombinations(occurrencesVariety)
+      .map { l => l.filter(_._2 > 0) }
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
